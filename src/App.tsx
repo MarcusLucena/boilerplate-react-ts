1818f26c8ad11bf7630316dd89ce18bdf95997b2
marcus.lucena.3.glo <marcus.lucena.3.glo@globalhitss.com.br>
2023-02-21 18:35:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+          Boilerplate React
+          <br />
+          <small>by Marcus Lucena</small>
+      </div>
+    </div>
+  )
+}
+
+export default App
